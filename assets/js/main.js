87d7d308805f3c2a3cf417a44d8a5d5086b373b8
i6rahim <i6rahim210@gmail.com>
2023-08-16 17:27:48 +0300

    "add"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,31 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
+    const imgMix = {
+        id: '1',
+        
+    }
 
     function homeContent() {
 
         const btns = document.querySelectorAll('.home__tab');
-        const imgContent = document.querySelectorAll('.home__content-img');
         const btnSize = document.querySelectorAll('.home__tabs-size');
-        
+        const bigImg = 'big';
+        let price = {
+            big: 399,
+            medium: 285,
+            small: 140,
+        }
+        const size = '';
         const images = 
-            {
-                imgMix: {
-                    default: 'assets/img/Mix big.webp',
-                    price: 399,
-                    size: {
-                        big: 'assets/img/Mix big.webp',
-                        medium: 'assets/img/Mix medium.webp',
-                        small: 'assets/img/Mix small.webp',
-                    },
-                },
-                imgNuts: {
-                    default: 'assets/img/only nuts big.webp',
-                    price: 399,
-                    size: {
-                        big: 'assets/img/only nuts big.webp',
-                        medium: 'assets/img/only nuts medium.webp',
-                        small: 'assets/img/only nuts small.webp',
-                    },
-                },
-                imgDates: {
-                    default: 'assets/img/Dates big.webp',
-                    price: 399,
-                    size: {
-                        big: 'assets/img/Dates big.webp',
-                        medium: 'assets/img/Dates medium.webp',
-                        small: 'assets/img/Dates small.webp',
-                    },
-                }
+        {
+                imgMix: `assets/img/Mix ${size}.webp`,
+                imgNuts: `assets/img/only nuts.webp`,
+                imgDates: `assets/img/Dates big.webp`,
             };
-
-        document.querySelector('.home__content-img').src = images.imgMix.default
-
-
-        btns.forEach(item => {
+            
+            console.log(size);
+            
+            btns.forEach(item => {
 
 
             item.addEventListener('click', ()=> {
@@ -56,60 +40,73 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 }       
                 currentBtn.classList.add('active')
                 if(btnId === "#mix") {
-                    document.querySelector('.home__content-img').src = images.imgMix.default
+                    document.querySelector('.home__content-img').src = images.imgMix
                 } else if (btnId === "#nuts") {
-                    document.querySelector('.home__content-img').src = images.imgNuts.default
+                    document.querySelector('.home__content-img').src = images.imgNuts
                 } else if (btnId === "#dates") {
-                    document.querySelector('.home__content-img').src = images.imgDates.default  
+                    document.querySelector('.home__content-img').src = images.imgDates 
                 }
-
+                
                 btnSize.forEach(e => {
                     e.addEventListener('click', ()=> {
-        
+                        const homePrice = document.querySelector('.home__price');
                         const currentBtnSize = e;
                         const currentBtnSizeId = currentBtnSize.getAttribute("data-size");
                         const currentImgSize = document.querySelector(currentBtnSizeId)
-    
+                        
                         if(! currentBtnSize.classList.contains('active')) {
                             btnSize.forEach(item => {
                                 item.classList.remove('active')
                             })
                         }
 
-    
-                        if(item.classList.contains('active')) {
-
-                            if(btnId === "#mix" && currentBtnSizeId === "#big") {
-                                document.querySelector('.home__content-img').src = images.imgMix.size.big
-                            } else if (btnId === "#mix" && currentBtnSizeId === "#medium") {
-                                document.querySelector('.home__content-img').src = images.imgMix.size.medium
-                            } else if (btnId === "#mix" && currentBtnSizeId === "#small") {                                                             
-                                document.querySelector('.home__content-img').src = images.imgMix.size.small
-                            }
+                        if(currentBtnSizeId === "#big") {
+                            homePrice.innerHTML = price.big;
+                        } else if (currentBtnSizeId === "#medium") {
+                            homePrice.innerHTML = price.medium;
+                        } else if (currentBtnSizeId === "#small") {
+                            homePrice.innerHTML = price.small;
                         }
-
+                        
+    
+                        // if(item.classList.contains('active')) {
+                            
+                        //     if(btnId === "#mix" && currentBtnSizeId === "#big") {
+                        //         document.querySelector('.home__content-img').src = images.size.big
+                        //     } else if (btnId === "#mix" && currentBtnSizeId === "#medium") {
+                            //         document.querySelector('.home__content-img').src = images.size.medium
+                            //     } else if (btnId === "#mix" && currentBtnSizeId === "#small") {                                                             
+                        //         document.querySelector('.home__content-img').src = images.size.small
+                        //     }
+                        // }
+                        
                         if(item.classList.contains('active')) {
 
                             if(btnId === "#nuts" && currentBtnSizeId === "#big") {
-                                document.querySelector('.home__content-img').src = images.imgNuts.size.big
+                                size = 'big',
+                                document.querySelector('.home__content-img').src = size;
+                                console.log(size);
                             } else if (btnId === "#nuts" && currentBtnSizeId === "#medium") {
-                                document.querySelector('.home__content-img').src = images.imgNuts.size.medium
-                            } else if (btnId === "#nuts" && currentBtnSizeId === "#small") {                                                             
-                                document.querySelector('.home__content-img').src = images.imgNuts.size.small
+                                size = 'medium',
+                                document.querySelector('.home__content-img').src = size
+
+                            } else if (btnId === "#nuts" && currentBtnSizeId === "#small") {
+                                innerImgSize = size.small;                                                             
+                                // document.querySelector('.home__content-img').src = images.size.small
                             }
                         }
 
 
-                        if(item.classList.contains('active')) {
+                        // if(item.classList.contains('active')) {
 
-                            if(btnId === "#dates" && currentBtnSizeId === "#big") {
-                                document.querySelector('.home__content-img').src = images.imgDates.size.big
-                            } else if (btnId === "#dates" && currentBtnSizeId === "#medium") {
-                                document.querySelector('.home__content-img').src = images.imgDates.size.medium
-                            } else if (btnId === "#dates" && currentBtnSizeId === "#small") {                                                             
-                                document.querySelector('.home__content-img').src = images.imgDates.size.small
-                            }
-                        }
+                        //     if(btnId === "#dates" && currentBtnSizeId === "#big") {
+                        //         document.querySelector('.home__content-img').src = images.size.big
+                        //     } else if (btnId === "#dates" && currentBtnSizeId === "#medium") {
+                        //         document.querySelector('.home__content-img').src = images.size.medium
+                        //     } else if (btnId === "#dates" && currentBtnSizeId === "#small") {                                                             
+                        //         document.querySelector('.home__content-img').src = images.size.small
+                        //     }
+                        // }
 
                         currentBtnSize.classList.add('active')
                     })
@@ -123,24 +120,37 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         })
 
-
     }
     homeContent()
+    
     document.querySelector('.home__tab').click();
     document.querySelector('.home__tabs-size').click();
 
 
+    
+    
+    
 
+
+
+
+
+
+    
+    
+    
+    
+    
     function deliveryBag() {
         const btns = document.querySelectorAll('.deliver__tab');
         const img = document.querySelectorAll('.deliver__content-img');
-
+    
         btns.forEach(item => {
             item.addEventListener('click', () => {
                 const currentBtn = item;
                 const currentBtnId = currentBtn.getAttribute('data-del');
                 const currentImg = document.querySelector(currentBtnId)    
-
+    
                     if(! currentBtn.classList.contains('active')) {
                         btns.forEach(item => {
                             item.classList.remove('active')
@@ -157,16 +167,16 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
     deliveryBag()
     document.querySelector('.deliver__tab').click();
-
-
-
-
-
+    
+    
+    
+    
+    
     function quantity() {
         const plus = document.querySelector('.quantity__plus');
         const result = document.querySelector('.quantity__numbre');
         const minus = document.querySelector('.quantity__minus');
-
+    
         plus.addEventListener('click', () => {
             result.value++
         });
@@ -179,16 +189,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
         })
     }
     quantity()
-
-
+    
+    
     function order() {
         const cart  = document.querySelectorAll('.cart');
         cart.forEach(item => {
-            console.log(item);
+    
             const plus = item.querySelector('.cart__plus');
             const result = item.querySelector('.cart__result-inner');
             const minus = item.querySelector('.cart__minus');
-            console.log(plus);
             
             plus.addEventListener('click', () => {
                 result.value++
@@ -201,7 +210,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 }
             })
         })
-
+    
     }
     order()
 
